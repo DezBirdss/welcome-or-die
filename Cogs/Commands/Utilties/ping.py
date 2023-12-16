57b8597ed0508efea0ab0122922bc6ce05982a32
Bugs <deezbird2768@gmail.com>
2023-12-16 22:03:28 +0000

    I DIDNT NEED FROM DATE TIME FR+AWIDAWD)

--- Cogs/Commands/Utilties/ping.py
+++ Cogs/Commands/Utilties/ping.py
@@ -7,7 +7,6 @@ from datetime import timedelta
 import asyncio
 from discord import app_commands
 from discord.ext import commands, tasks
-from datetime import datetime
 import platform
 
 class ping(commands.Cog):
@@ -21,7 +20,7 @@ class ping(commands.Cog):
         server_icon = ctx.guild.icon
         discord_latency = self.client.latency * 1000
         discord_latency_message = f"**Latency:** {discord_latency:.0f}ms"
-        embed = discord.Embed(title="<:Network:1185700803747516457> Network Information", description=f"{discord_latency_message}\n**Uptime:** <t:{int(self.client.launch_time.timestamp())}:R>", color=0x2b2d31)
+        embed = discord.Embed(title="<:Network:1185700803747516457> Network Information", description=f"{discord_latency_message}\n**Uptime:** <t:{int(self.launch_time.timestamp())}:R>", color=0x2b2d31)
         embed.set_author(name=server_name, icon_url=server_icon)
         embed.set_thumbnail(url=ctx.guild.icon)
         await ctx.send(embed=embed)
